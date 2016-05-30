dccb72c11af517b57a81031810fb4e82bd46a543
John Wood <johnrobertwood@yahoo.com>
2016-05-30 10:42:36 -0700

    First commit

---
+++ config/auth.js
@@ -0,0 +1,22 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '136653380075831', // your App ID
+        'clientSecret'  : 'ed310996df45f37dc4cd7fad775d2ed2', // your App Secret
+        'callbackURL'   : 'http://oldtimesublime.com'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    }
+
+};
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'url' :  'mongodb://'+process.env.USER+':'+process.env.MONGO_PW+'@ds019033.mlab.com:19033/nodeauth'
+};
